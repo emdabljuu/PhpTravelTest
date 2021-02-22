9b4a8a7b717848bde4e25ed883249d5dff9ac23a
Marcin Wiewiora <marcin_wiewiora@wp.pl>
2021-02-22 21:09:07 +0100

    22.02.2021 21:08 Getting first search results

--- src/main/java/com/travelers/pages/HomePage.java
+++ src/main/java/com/travelers/pages/HomePage.java
@@ -1,10 +1,13 @@
 package com.travelers.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 //Class contains HomePage's Web Elements
 public class HomePage {
@@ -14,12 +17,27 @@ public class HomePage {
 
     @FindBy (xpath = "//*[@id=\"select2-drop\"]/div/input")
     private WebElement searchCityInput;
-    //Find checkin date
+    //find checkin date
     @FindBy (name = "checkin")
     private WebElement checkIn;
     //find checkout date
     @FindBy (name = "checkout")
     private  WebElement checkOut;
+    //find travellers
+    @FindBy (id = "travellersInput")
+    private  WebElement travellersInput;
+    //find Adults
+    @FindBy (name = "adults")
+    private  WebElement adultsNumber;
+    //find Child
+    @FindBy (name = "child")
+    private  WebElement childNumber;
+    //Find search Button
+    @FindBy (xpath = "//*[@id=\"hotels\"]/form/div[5]/button")
+    private WebElement searchButton;
+    //Find table with results
+    @FindBy (xpath = "//table[@class='bgwhite table table-striped']")
+    private WebElement resultsTable;
 
     //Constructor for class HomePage
     public HomePage(WebDriver driver) {
@@ -39,8 +57,38 @@ public class HomePage {
         checkIn.sendKeys(checkInDate);
         //checkOut.click();
         checkOut.sendKeys(checkOutDate);
-        checkOut.click();
 
     }
+    //Public class, set number of travellers by sendKeys
+    public void setTravellersInput(String adults, String child){
+        travellersInput.click();
+        //Clear default values and set adults number
+        adultsNumber.clear();
+        adultsNumber.sendKeys(adults);
+        //Clear default values and set child number
+        childNumber.clear();
+        childNumber.sendKeys(child);
+    }
 
+    //Public class, start searching
+    public void startSearch(){
+        searchButton.click();
+    }
+
+    //Public class, analyze table with results
+    public void getHotelNames(){
+        List<WebElement> hotelNames = resultsTable.findElements(By.xpath("//h4//b"));
+        for(WebElement hotelName: hotelNames) {
+                System.out.println(hotelName.getText());
+            }
+        }
+
+    //Public class, get Hotel Prices
+    public void getHotelPrices() {
+        List<WebElement> hotelPrices = resultsTable.findElements(By.xpath("//div[contains(@class,'price_tab')]//b"));
+        hotelPrices.stream().forEach(hotelPrice ->
+                System.out.println(hotelPrice.getText()));
+    }
 }
+
+
--- src/main/java/com/travelers/tests/SearchHotelTest.java
+++ src/main/java/com/travelers/tests/SearchHotelTest.java
@@ -3,11 +3,15 @@ package com.travelers.tests;
 import com.travelers.pages.HomePage;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 
 public class SearchHotelTest extends BaseSeleniumTest{
     //Main Test
     @Test
     public void searchHotelTest() throws InterruptedException {
+        //Wait for website
+        driver.manage().timeouts().implicitlyWait(15L, TimeUnit.SECONDS);
         //Getting correct website address
         String website = "http://www.kurs-selenium.pl/demo/";
         //Hand website name over to driver
@@ -15,8 +19,20 @@ public class SearchHotelTest extends BaseSeleniumTest{
         //Create new HomePage object
         HomePage homePage = new HomePage(driver);
         //Methods execution on homePage
-        homePage.setCityName("Miami");
+        //Add cityName to constructor
+        homePage.setCityName("Dubai");
+        //Add dateRange to constructor
         homePage.setDateRange("01/02/2018", "07/02/2018");
+        //Add travellers to constructor
+        homePage.setTravellersInput("6","3");
+        //Start searching
+        homePage.startSearch();
+        //Get hotel Names
+        Thread.sleep(4000);
+        homePage.getHotelNames();
+        Thread.sleep(4000);
+        //Get hotels prices
+        homePage.getHotelPrices();
 
     }
 }
